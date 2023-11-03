9c01fda6ab4926a0403f5c55fb5009c2a017fd0f
keeepmin94 <wjdwkjh94@gmail.com>
2023-11-03 19:31:14 +0900

    feat: refresh token 로직 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,28 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common'
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  Get,
+  Request,
+} from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { SignInDto } from './dto/signin.dto'
+import { UseGuards } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/signin')
-  signIn(
-    @Body(ValidationPipe) signInDto: SignInDto,
-  ): Promise<{ accessToken: string }> {
+  signIn(@Body(ValidationPipe) signInDto: SignInDto): Promise<object> {
     return this.authService.signIn(signInDto)
+  }
+
+  @UseGuards(AuthGuard('jwt-refresh-token'))
+  @Get('/refresh')
+  getRefreshToken(@Request() req): Promise<{ accessToken: string }> {
+    return this.authService.getNewAccessToken(req.user)
   }
 }
