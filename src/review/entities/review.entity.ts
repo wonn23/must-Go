eae85e1225089834290ec19954f6e054513032ab
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-11-04 12:47:22 +0900

    docs: 리뷰 엔터티 스웨거 문서 적용

--- src/review/entities/review.entity.ts
+++ src/review/entities/review.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { Restaurant } from 'src/restaurant/entities/restaurant.entity'
 import { User } from 'src/user/entities/user.entity'
 import {
@@ -15,26 +16,34 @@ import {
 @Entity()
 export class Review extends BaseEntity {
   @PrimaryGeneratedColumn()
+  @ApiProperty({ description: '리뷰 아이디' })
   id: number
 
   @Column({ type: 'double precision' })
+  @ApiProperty({ description: '리뷰 점수' })
   score: number
 
   @Column()
+  @ApiProperty({ description: '리뷰 내용' })
   content: string
 
   @CreateDateColumn({ nullable: false })
+  @ApiProperty({ description: '리뷰 생성 일자' })
   createdAt: Date
 
   @UpdateDateColumn({ nullable: false })
+  @ApiProperty({ description: '리뷰 수정 일자' })
   updatedAt: Date
 
   @DeleteDateColumn({ nullable: false })
+  @ApiProperty({ description: '리뷰 삭제 일자' })
   deletedAt?: Date
 
   @ManyToOne(() => User, (user) => user.reviews)
+  @ApiProperty({ description: '리뷰 작성자' })
   user: User
   @ManyToOne(() => Restaurant, (restaurant) => restaurant.reviews)
   @JoinColumn({ name: 'restaurantId' }) // 외래 키 설정
+  @ApiProperty({ description: '리뷰한 맛집' })
   restaurant: Restaurant
 }
