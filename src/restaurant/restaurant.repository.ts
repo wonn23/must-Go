c2d3131dbc4466159f973df87fb759ee4291ea80
[AI7/2반] 이은석 <zhes4593@gmail.com>
2023-11-07 10:59:00 +0900

    refact: point 간 거리를 raw query로 계산

--- src/restaurant/restaurant.repository.ts
+++ src/restaurant/restaurant.repository.ts
@@ -1,30 +1,47 @@
-import { Between, Repository } from 'typeorm'
+import { Between, Repository, getRepository } from 'typeorm'
 import { CustomRepository } from '../common/decorator/typeorm-ex.decorator'
 import { Restaurant } from './entities/restaurant.entity'
 import { statusEnum } from './types/restaurant.enum'
+
 @CustomRepository(Restaurant)
 export class RestaurantRepository extends Repository<Restaurant> {
-  async getRestaurantsInRange(query): Promise<Restaurant[]> {
-    const { lat, lon, range, filter } = query
+  async getRestaurantsInRange(query): Promise<object[]> {
+    const { lat, lon, range, orderBy, filter } = query
+    const latRange: number = range * 0.009009 // 대략적으로 1km에 0.009009도
+    const lonRange: number = range * 0.011236 // 대략적으로 1km에 0.011236도
+    const minLat: number = lat - latRange
+    const maxLat: number = lat + latRange
+    const minLon: number = lon - lonRange
+    const maxLon: number = lon + lonRange
 
-    // 위도와 경도에 범위를 더하거나 뺀 값으로 사각형의 네 꼭짓점 구하기
-    const latRange = range / 111 // 대략적으로 1도는 111km
-    const lonRange = range / 88 // 대략적으로 1도는 88km
+    const queryBuilder = this.createQueryBuilder('restaurant')
+      // 사각형 범위로 필터링
+      .where(`lat::float BETWEEN ${minLat} AND ${maxLat}`)
+      .andWhere(`lon::float BETWEEN ${minLon} AND ${maxLon}`)
 
-    const minLat = lat - latRange
-    const maxLat = lat + latRange
-    const minLon = lon - lonRange
-    const maxLon = lon + lonRange
+    // 유형에 따른 필터링
+    if (filter) {
+      queryBuilder.andWhere(`restaurant.type = :filter`, { filter })
+    }
 
-    // 데이터베이스에서 사각형 범위에 있는 영업 중인 식당을 찾음.
-    const restaurants = await this.find({
-      where: {
-        lat: Between(minLat, maxLat),
-        lon: Between(minLon, maxLon),
-        status: statusEnum.open,
-        type: filter,
-      },
-    })
+    // 하버사인 공식을 이용한 거리 계산
+    queryBuilder.addSelect(
+      `6371 * acos(cos(radians(${lat})) * cos(radians(restaurant.lat::float)) * cos(radians(restaurant.lon::float) - radians(${lon})) + sin(radians(${lat})) * sin(radians(restaurant.lat::float)))`,
+      'distance',
+    )
+
+    // status가 '영업'인 식당만 필터링
+    queryBuilder.andWhere(`restaurant.status = '영업'`)
+
+    // 정렬
+    if (orderBy === 'Rating') {
+      queryBuilder.orderBy('restaurant.score', 'DESC')
+    } else if (orderBy === 'Distance') {
+      queryBuilder.orderBy('distance', 'ASC')
+    }
+
+    // 쿼리 실행
+    const restaurants = await queryBuilder.getRawMany()
     return restaurants
   }
 
